54819ad847b739babf0c65afc170d40649fd2321
Paul Theriault <pauljt@mozilla.com>
2018-03-16 15:17:22 +1100

    fix }

--- js/client.js
+++ js/client.js
@@ -122,7 +122,6 @@ var getBadges = function(t){
     }
   });
   */
-};
 
 // We need to call initialize to get all of our capability handles set up and registered with Trello
 TrelloPowerUp.initialize({
